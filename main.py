7854a8376108e6d287f5571e04771a74546e8672
Stefano Peris <lordstephen77@gmail.com>
2017-10-21 09:04:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 * @copyright   Stefano Peris (c) 2017
 * @author      Stefano Peris
 * @email       lordstephen77@gmail.com
-* @github      https://github.com/LordStephen77/Return-of-Dungeon
+* @github      https://github.com/LordStephen77/PyWebServer
 * @license     GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 * @create      lun 21 ott 2017 09:03:43 CEST
 * @update      null
