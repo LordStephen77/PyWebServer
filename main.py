3100ae7ba483de8df37a12c7e57698684f6d9597
Stefano Peris <lordstephen77@gmail.com>
2017-10-21 09:03:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+* @name        PyWebServer
+* @copyright   Stefano Peris (c) 2017
+* @author      Stefano Peris
+* @email       lordstephen77@gmail.com
+* @github      https://github.com/LordStephen77/Return-of-Dungeon
+* @license     GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+* @create      lun 21 ott 2017 09:03:43 CEST
+* @update      null
+"""
 
 from twisted.web import server, resource
 from twisted.internet import reactor, endpoints
